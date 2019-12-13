628dfe7f4da5f506718310db93aecf4e02a39690
jo-branda <jpab1998@hotmail.com>
2019-12-13 14:23:03 +0000

    Object Class

--- trabalho_cg_tema2/Form1.Designer.cs
+++ trabalho_cg_tema2/Form1.Designer.cs
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pb_curvesDisplay = new System.Windows.Forms.PictureBox();
+            this.pb_modelsDisplay = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_curvesDisplay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_modelsDisplay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pb_curvesDisplay
+            // 
+            this.pb_curvesDisplay.Location = new System.Drawing.Point(36, 44);
+            this.pb_curvesDisplay.Name = "pb_curvesDisplay";
+            this.pb_curvesDisplay.Size = new System.Drawing.Size(414, 310);
+            this.pb_curvesDisplay.TabIndex = 0;
+            this.pb_curvesDisplay.TabStop = false;
+            // 
+            // pb_modelsDisplay
+            // 
+            this.pb_modelsDisplay.Location = new System.Drawing.Point(495, 44);
+            this.pb_modelsDisplay.Name = "pb_modelsDisplay";
+            this.pb_modelsDisplay.Size = new System.Drawing.Size(558, 310);
+            this.pb_modelsDisplay.TabIndex = 1;
+            this.pb_modelsDisplay.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1079, 624);
+            this.Controls.Add(this.pb_modelsDisplay);
+            this.Controls.Add(this.pb_curvesDisplay);
+            this.Name = "Form1";
+            this.Text = "Visual3D all night";
+            ((System.ComponentModel.ISupportInitialize)(this.pb_curvesDisplay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_modelsDisplay)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pb_curvesDisplay;
+        private System.Windows.Forms.PictureBox pb_modelsDisplay;
     }
 }
 
